cd63697df3e033397818161c9f760baf2495d900
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-03-01 20:07:31 -0300

    refactor(contract): creates new variables and change functions visibility
    create the new variables bid and comission to allow changes after deployment, and change functions visibility to external, to reduce the gas cost

--- blockchain/contracts/JoKenPo.sol
+++ blockchain/contracts/JoKenPo.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.20;
 
-//Módulo 02 > Lição 04 > Tóps 07 e 08 - JoKenPo com LeaderBoard
 contract JoKenPo {
 
-    enum Options { NONE, ROCK, PAPER, SCISSORS }//0, 1, 2, 3
+    enum Options { NONE, ROCK, PAPER, SCISSORS } // 0, 1, 2, 3
 
     Options private choice1 = Options.NONE;
     address private player1;
-    string public result = "";
+    string private result = "";
+    uint256 private bid = 0.01 ether;
+    uint8 private comission = 10; // percent
 
     address payable private immutable owner;
 
@@ -22,6 +22,30 @@ contract JoKenPo {
 
     constructor(){
         owner = payable(msg.sender);
+    }
+
+    function getResult() external view returns (string memory) {
+        return result;
+    }
+
+    function getBid() external view returns (uint256) {
+        return bid;
+    }
+    
+    function getComission() external view returns (uint8) {
+        return comission;
+    }
+
+    function setBid(uint256 _bid) external {
+        require(msg.sender == owner, "You do not have permission");
+        require(player1 == address(0), "You cannot change the bid with a game in progress");
+        bid = _bid;
+    }
+
+    function setComission(uint8 _comission) external {
+        require(msg.sender == owner, "You do not have permission");
+        require(player1 == address(0), "You cannot change the comission with a game in progress");
+        comission = _comission;
     }
 
     function updateWinner(address winner) private {
@@ -37,7 +61,7 @@ contract JoKenPo {
 
     function finishGame(string memory newResult, address winner) private {
         address contractAddress = address(this);
-        payable(winner).transfer((contractAddress.balance / 100) * 90);
+        payable(winner).transfer((contractAddress.balance / 100) * (100 - comission));
         owner.transfer(contractAddress.balance);
 
         updateWinner(winner);
@@ -48,40 +72,41 @@ contract JoKenPo {
     }
 
     function getBalance() public view returns(uint) {
-        require(owner == msg.sender, "You don't have this permission.");
+        require(owner == msg.sender, "You do not have this permission");
         return address(this).balance;
     }
 
-    function play(Options newChoice) public payable {
+    function play(Options newChoice) external payable {
+        require(msg.sender != owner, "The owner cannot play");
         require(newChoice != Options.NONE, "Invalid choice");
-        require(player1 != msg.sender, "Wait the another player.");
-        require(msg.value >= 0.01 ether, "Invalid bid.");
+        require(player1 != msg.sender, "Wait the another player");
+        require(msg.value >= bid, "Invalid bid");
 
         if(choice1 == Options.NONE){
             player1 = msg.sender;
             choice1 = newChoice;
-            result = "Player 1 choose his/her option. Waiting player 2.";
+            result = "Player 1 choose his/her option. Waiting player 2";
         }
         else if(choice1 == Options.ROCK && newChoice == Options.SCISSORS)
-            finishGame("Rock breaks scissors. Player 1 won.", player1);
+            finishGame("Rock breaks scissors. Player 1 won", player1);
         else if(choice1 == Options.PAPER && newChoice == Options.ROCK)
-            finishGame("Paper wraps rock. Player 1 won.", player1);
+            finishGame("Paper wraps rock. Player 1 won", player1);
         else if(choice1 == Options.SCISSORS && newChoice == Options.PAPER)
-            finishGame("Scissors cuts paper. Player 1 won.", player1);
+            finishGame("Scissors cuts paper. Player 1 won", player1);
         else if(choice1 == Options.SCISSORS && newChoice == Options.ROCK)
-            finishGame("Rock breaks scissors. Player 2 won.", msg.sender);
+            finishGame("Rock breaks scissors. Player 2 won", msg.sender);
         else if(choice1 == Options.ROCK && newChoice == Options.PAPER)
-            finishGame("Paper wraps rock. Player 2 won.", msg.sender);
+            finishGame("Paper wraps rock. Player 2 won", msg.sender);
         else if(choice1 == Options.PAPER && newChoice == Options.SCISSORS)
-            finishGame("Scissors cuts paper. Player 2 won.", msg.sender);
+            finishGame("Scissors cuts paper. Player 2 won", msg.sender);
         else {
-            result = "Draw game. The prize was doubled.";
+            result = "Draw game. The prize was doubled";
             player1 = address(0);
             choice1 = Options.NONE;
         }
     }
 
-    function getLeaderboard() public view returns(Player[] memory){
+    function getLeaderboard() external view returns(Player[] memory){
         if(players.length < 2) return players;
 
         Player[] memory arr = new Player[](players.length);
