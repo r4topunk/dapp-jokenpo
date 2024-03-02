515d8a2b98229af72bbbfdea4e43c0e7b611b3fb
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-03-02 00:02:44 -0300

    feat: add adapter

---
+++ blockchain/contracts/JKPAdapter.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "./IJoKenPo.sol";
+
+contract JKPAdapter {
+
+  IJoKenPo private joKenPo;
+  address public immutable owner;
+
+  constructor() {
+    owner = msg.sender;
+  }
+
+  function upgrade(address _contract) external {
+    require(msg.sender == owner, "You do not have permission");
+    require(_contract != address(0), "Empty address is not permitted");
+    joKenPo = IJoKenPo(_contract);
+  }
+  
+}
