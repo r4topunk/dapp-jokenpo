a1fe8dd05ef95466b6c245abef60fc52abbc67a9
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-03-03 08:58:11 -0300

    feat: add admin functions

--- front-end/src/Admin.tsx
+++ front-end/src/Admin.tsx
@@ -1,10 +1,146 @@
+import { useEffect, useState } from "react"
 import { Header } from "./Header"
+import {
+  Dashboard,
+  getDashboard,
+  setBid,
+  setCommission,
+  upgrade,
+} from "./Web3Service"
 
 function Admin() {
+  const [message, setMessage] = useState("")
+  const [dashboard, setDashboard] = useState<Dashboard>()
+
+  useEffect(() => {
+    getDashboard()
+      .then((dashboard) => {
+        console.log("dashboard", dashboard)
+        return setDashboard(dashboard)
+      })
+      .catch((err) => setMessage(err.message))
+  }, [])
+
+  function onInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setDashboard((prevState) => ({
+      ...prevState,
+      [event.target.id]: event.target.value,
+    }))
+  }
+
+  function onUpgradeClick(): void {
+    if (!dashboard?.address) return setMessage("Address is required")
+
+    upgrade(dashboard.address)
+      .then((tx) => setMessage("Success. Tx: " + tx))
+      .catch((err) => setMessage(err.message))
+  }
+
+  function onChangeBidClick(): void {
+    if (!dashboard?.bid) return setMessage("Bid is required")
+
+    setBid(dashboard.bid)
+      .then((tx) => setMessage("Success. Tx: " + tx))
+      .catch((err) => setMessage(err.message))
+  }
+
+  function onChangeCommissionClick(): void {
+    if (!dashboard?.commission) return setMessage("Commission is required")
+
+    setCommission(dashboard.commission)
+      .then((tx) => setMessage("Success. Tx: " + tx))
+      .catch((err) => setMessage(err.message))
+  }
+
   return (
     <div className="container">
       <Header />
-      <main>Admin page</main>
+      <main>
+        <div className="py-5 text-center">
+          <img
+            className="d-block mx-auto mb-4"
+            src="/logo512.png"
+            alt="JoKenPo"
+            width="72"
+          />
+          <h2>Administrative Panel</h2>
+          <p className="lead">
+            Change the players' bid, your commission and upgrade the contract.
+          </p>
+          <p className="lead text-danger">{message}</p>
+        </div>
+        <div className="col-md-8 col-lg-12">
+          <div className="row">
+            <div className="col-sm-6">
+              <label htmlFor="bid" className="form-label">
+                Bid (wei):
+              </label>
+              <div className="input-group">
+                <input
+                  type="number"
+                  className="form-control"
+                  id="bid"
+                  value={dashboard?.bid || ""}
+                  onChange={onInputChange}
+                />
+                <span className="input-group-text bg-secondary">wei</span>
+                <button
+                  type="button"
+                  className="btn btn-primary d-inline-flex align-items-center"
+                  onClick={onChangeBidClick}
+                >
+                  Change Bid
+                </button>
+              </div>
+            </div>
+            <div className="col-sm-6">
+              <label htmlFor="commission" className="form-label">
+                Commission (%):
+              </label>
+              <div className="input-group">
+                <input
+                  type="number"
+                  className="form-control"
+                  id="commission"
+                  value={dashboard?.commission || ""}
+                  onChange={onInputChange}
+                />
+                <span className="input-group-text bg-secondary">%</span>
+                <button
+                  type="button"
+                  className="btn btn-primary d-inline-flex align-items-center"
+                  onClick={onChangeCommissionClick}
+                >
+                  Change Commission
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="row py-5">
+            <div className="col-sm-12">
+              <label htmlFor="address" className="form-label">
+                New Contract (address):
+              </label>
+              <div className="input-group">
+                <input
+                  type="text"
+                  className="form-control"
+                  id="address"
+                  value={dashboard?.address || ""}
+                  onChange={onInputChange}
+                />
+                <button
+                  type="button"
+                  className="btn btn-primary d-inline-flex align-items-center"
+                  onClick={onUpgradeClick}
+                >
+                  Upgrade Contract
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   )
 }
--- front-end/src/Web3Service.ts
+++ front-end/src/Web3Service.ts
@@ -1,4 +1,5 @@
 import Web3, { Contract } from "web3"
+import { AbiType } from "./AbiType"
 import ABI from "./abi.json"
 
 const ADAPTER_ADDRESS = `${process.env.REACT_APP_CONTRACT_ADDRESS}`
@@ -12,7 +13,7 @@ function getWeb3(): Web3 {
 
 function getContract(web3?: Web3): Contract<typeof ABI> {
   if (!web3) web3 = getWeb3()
-  return new web3.eth.Contract(ABI, ADAPTER_ADDRESS, {
+  return new web3.eth.Contract(ABI as AbiType, ADAPTER_ADDRESS, {
     from: localStorage.getItem(LS_KEY_ACCOUNT) || undefined,
   })
 }
@@ -22,7 +23,7 @@ type LoginResult = {
   isAdmin: boolean
 }
 
-async function doLogin(): Promise<LoginResult> {
+export async function doLogin(): Promise<LoginResult> {
   const web3 = new Web3(window.ethereum)
   const accounts = await web3.eth.requestAccounts()
 
@@ -34,7 +35,7 @@ async function doLogin(): Promise<LoginResult> {
   const isAdmin = ownerAddress.toLowerCase() === accounts[0].toLowerCase()
 
   localStorage.setItem(LS_KEY_ACCOUNT, accounts[0])
-  localStorage.setItem(LS_KEY_IS_ADMIN, `${accounts[0] === ownerAddress}`)
+  localStorage.setItem(LS_KEY_IS_ADMIN, `${isAdmin}`)
 
   return {
     account: accounts[0],
@@ -42,17 +43,60 @@ async function doLogin(): Promise<LoginResult> {
   } as LoginResult
 }
 
-function doLogout() {
+export function doLogout() {
   localStorage.removeItem(LS_KEY_ACCOUNT)
   localStorage.removeItem(LS_KEY_IS_ADMIN)
 }
 
-function getLocalAccount() {
+export function getLocalAccount() {
   return localStorage.getItem(LS_KEY_ACCOUNT)
 }
 
-function getLocalIsAdmin() {
+export function getLocalIsAdmin() {
   return localStorage.getItem(LS_KEY_IS_ADMIN) === "true"
 }
 
-export { doLogin, doLogout, getLocalAccount, getLocalIsAdmin }
+export type Dashboard = {
+  bid?: string
+  commission?: number
+  address?: string
+}
+
+export async function getDashboard(): Promise<Dashboard> {
+  const contract = getContract()
+  const address = (await contract.methods.getContractAddress().call()) as string
+
+  if (/^(0x0)+$/.test(address))
+    return {
+      bid: Web3.utils.toWei("0.01", "ether"),
+      commission: 10,
+      address,
+    } as Dashboard
+
+  const bid = await contract.methods.getBid().call()
+  const commission = await contract.methods.getCommission().call()
+
+  return {
+    bid,
+    commission,
+    address,
+  } as Dashboard
+}
+
+export async function upgrade(newContract: string) {
+  const contract = getContract()
+  const tx = await contract.methods.upgrade(newContract).send()
+  return tx.transactionHash
+}
+
+export async function setCommission(newCommission: number) {
+  const contract = getContract()
+  const tx = await contract.methods.setCommission(newCommission).send()
+  return tx.transactionHash
+}
+
+export async function setBid(newBid: string) {
+  const contract = getContract()
+  const tx = await contract.methods.setBid(newBid).send()
+  return tx.transactionHash
+}
