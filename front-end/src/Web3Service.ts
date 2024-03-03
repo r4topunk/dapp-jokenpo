7fc083e8ba5e5c1b0526dcf10bcb23e1e82f25c8
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-03-02 21:18:08 -0300

    refactor: code organization

--- front-end/src/Web3Service.ts
+++ front-end/src/Web3Service.ts
@@ -1,5 +1,19 @@
-import Web3 from "web3"
+import Web3, { Contract } from "web3"
 import ABI from "./abi.json"
+
+const ADAPTER_ADDRESS = `${process.env.REACT_APP_CONTRACT_ADDRESS}`
+
+function getWeb3(): Web3 {
+  if (!window.ethereum) throw new Error("No MetaMask found.")
+  return new Web3(window.ethereum)
+}
+
+function getContract(web3?: Web3): Contract<typeof ABI> {
+  if (!web3) web3 = getWeb3()
+  return new web3.eth.Contract(ABI, ADAPTER_ADDRESS, {
+    from: localStorage.getItem("account") || undefined,
+  })
+}
 
 type LoginResult = {
   account: string
@@ -7,19 +21,13 @@ type LoginResult = {
 }
 
 async function doLogin(): Promise<LoginResult> {
-  if (!window.ethereum) throw new Error("No MetaMask found.")
-
   const web3 = new Web3(window.ethereum)
   const accounts = await web3.eth.requestAccounts()
 
   if (!accounts || !accounts.length)
     throw new Error("Wallet not found/allowed.")
 
-  const contract = new web3.eth.Contract(
-    ABI,
-    process.env.REACT_APP_CONTRACT_ADDRESS,
-    { from: accounts[0] }
-  )
+  const contract = getContract(web3)
   const ownerAddress = (await contract.methods.owner().call()) as string
   const isAdmin = ownerAddress.toLowerCase() === accounts[0].toLowerCase()
 
